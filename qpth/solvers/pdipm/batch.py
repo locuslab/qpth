Remove lambda scaling from dlam. Correct get_step batched. Include the duality gap.

qpth/solvers/pdipm/batch.py
@@ -1,4 +1,5 @@
 import torch
+import sys
 from qpth.util import get_sizes
 
 def forward(inputs, Q, G, h, A, b, Q_LU, S_LU, R):
@@ -53,7 +54,7 @@ def forward(inputs, Q, G, h, A, b, Q_LU, S_LU, R):
         y_resid = torch.norm(ry, 2, 1).squeeze() if neq > 0 else 0
         pri_resid = y_resid + z_resid
         dual_resid = torch.norm(rx, 2, 1).squeeze()
-        resids = pri_resid+dual_resid
+        resids = pri_resid + dual_resid + nineq*mu
         if best['resids'] is None:
             best['resids'] = resids
             best['x'] = x.clone()
@@ -77,7 +78,7 @@ def forward(inputs, Q, G, h, A, b, Q_LU, S_LU, R):
                 I_neq = I.repeat(neq, 1).t()
                 best['y'][I_neq] = y[I_neq]
         d = z/s
-        if nNotImproved == 3 or best['resids'].max() < 1e-6:
+        if nNotImproved == 3 or best['resids'].max() < 1e-12:
             return best['x'], best['y'], best['z'], best['s']
 
         # L_Q, L_S, R_ = pre_factor_kkt(Q, G, A)
@@ -87,6 +88,7 @@ def forward(inputs, Q, G, h, A, b, Q_LU, S_LU, R):
         factor_kkt(S_LU, R, d)
         dx_aff, ds_aff, dz_aff, dy_aff = solve_kkt(
             Q_LU, d, G, A, S_LU, rx, rs, rz, ry)
+
         # D = diaged(d)
         # dx_aff1, ds_aff1, dz_aff1, dy_aff1 = factor_solve_kkt(
         #     Q, D, G, A, rx, rs, rz, ry)
@@ -129,10 +131,13 @@ def forward(inputs, Q, G, h, A, b, Q_LU, S_LU, R):
         ds = ds_aff + ds_cor
         dz = dz_aff + dz_cor
         dy = dy_aff + dy_cor if neq > 0 else None
-        # alpha0 = min(1.0, 0.999*min(get_step(s[0],ds[0]), get_step(z[0],dz[0])))
+        import qpth.solvers.pdipm.single as pdipm_s
+        alpha0 = min(1.0, 0.999*min(pdipm_s.get_step(s[0],ds[0]), pdipm_s.get_step(z[0],dz[0])))
         alpha = torch.min(0.999*torch.min(get_step(z, dz),
                                           get_step(s, ds)),
                           torch.ones(nBatch).type_as(Q))
+        assert(alpha0 - alpha[0] <= 1e-10) # TODO: Remove
+
         alpha_nineq = alpha.repeat(nineq, 1).t()
         alpha_neq = alpha.repeat(neq, 1).t() if neq > 0 else None
         alpha_nz = alpha.repeat(nz, 1).t()
@@ -154,7 +159,7 @@ def forward(inputs, Q, G, h, A, b, Q_LU, S_LU, R):
 def get_step(v,dv):
     nBatch = v.size(0)
     a = -v/dv
-    a[dv >= 1e-12] = a.max()
+    a[dv >= 1e-12] = max(1.0, a.max())
     return a.min(1)[0].squeeze()
 
 def factor_solve_kkt(Q, D, G, A, rx, rs, rz, ry):

qpth/solvers/pdipm/single.py
@@ -1,6 +1,8 @@
 import torch
 import numpy as np
 
+from qpth.util import get_sizes
+
 # TODO: Add more comments describing the math here.
 # https://stanford.edu/~boyd/papers/pdf/code_gen_impl.pdf
 
@@ -10,7 +12,7 @@ def forward(inputs_i, Q, G, A, b, h, U_Q, U_S, R):
     h = G z_0 + s_0
     U_Q, U_S, R = pre_factor_kkt(Q, G, A, nineq, neq)
     """
-    nineq, nz, neq, _ = getSizes(G, A)
+    nineq, nz, neq, _ = get_sizes(G, A)
 
     # find initial values
     d = torch.ones(nineq).type_as(Q)
@@ -39,11 +41,11 @@ def forward(inputs_i, Q, G, A, b, h, U_Q, U_S, R):
         mu = torch.dot(s,z)/nineq
         pri_resid = torch.norm(ry) + torch.norm(rz)
         dual_resid = torch.norm(rx)
-        resid = pri_resid+dual_resid
+        resid = pri_resid+dual_resid+nineq*mu
         d = z/s
-        # print(("primal_res = {0:.5g}, dual_res = {1:.5g}, " +
-        #         "gap = {2:.5g}, kappa(d) = {3:.5g}").format(
-        #             pri_resid, dual_resid, mu, min(d)/max(d)))
+        print(("primal_res = {0:.5g}, dual_res = {1:.5g}, " +
+                "gap = {2:.5g}, kappa(d) = {3:.5g}").format(
+                    pri_resid, dual_resid, mu, min(d)/max(d)))
         # if (pri_resid < 5e-4 and dual_resid < 5e-4 and mu < 4e-4):
         improved = (prev_resid is None) or (resid < prev_resid + 1e-6)
         if not improved or resid < 1e-6:
@@ -87,8 +89,8 @@ def forward(inputs_i, Q, G, A, b, h, U_Q, U_S, R):
     return x, y, z
 
 def get_step(v,dv):
-    I = dv < 0
-    if torch.sum(I) > 1e-12: # TODO: Use something like torch.any(dv < 0)
+    I = dv < 1e-12
+    if torch.sum(I) > 0: # TODO: Use something like torch.any(dv < 0)
         a = -v/dv
         return torch.min(a[I])
     else:
@@ -96,7 +98,7 @@ def get_step(v,dv):
 
 def solve_kkt(U_Q, d, G, A, U_S, rx, rs, rz, ry, dbg=False):
     """ Solve KKT equations for the affine step"""
-    nineq, nz, neq, _ = getSizes(G, A)
+    nineq, nz, neq, _ = get_sizes(G, A)
 
     invQ_rx = torch.potrs(rx.view(-1,1), U_Q).view(-1)
     if neq > 0:
@@ -125,7 +127,7 @@ def solve_kkt(U_Q, d, G, A, U_S, rx, rs, rz, ry, dbg=False):
 
 def pre_factor_kkt(Q, G, A):
     """ Perform all one-time factorizations and cache relevant matrix products"""
-    nineq, nz, neq, _ = getSizes(G, A)
+    nineq, nz, neq, _ = get_sizes(G, A)
 
     # S = [ A Q^{-1} A^T        A Q^{-1} G^T           ]
     #     [ G Q^{-1} A^T        G Q^{-1} G^T + D^{-1} ]
@@ -163,7 +165,7 @@ def factor_kkt(U_S, R, d):
     U_S[-nineq:,-nineq:] = torch.potrf(R + torch.diag(1/d.cpu()).type_as(d))
 
 def factor_solve_kkt(Q, D, G, A, rx, rs, rz, ry):
-    nineq, nz, neq, _ = getSizes(G, A)
+    nineq, nz, neq, _ = get_sizes(G, A)
 
     if neq > 0:
         H_ = torch.cat([torch.cat([Q, torch.zeros(nz,nineq).type_as(Q)], 1),

test.py
@@ -29,12 +29,15 @@ sys.path.append('..')
 import qpth
 import qpth.solvers.cvxpy as qp_cvxpy
 
-from IPython.core import ultratb
-sys.excepthook = ultratb.FormattedTB(mode='Verbose',
-     color_scheme='Linux', call_pdb=1)
+import qpth.solvers.pdipm.single as pdipm_s
+import qpth.solvers.pdipm.batch as pdipm_b
+
+# from IPython.core import ultratb
+# sys.excepthook = ultratb.FormattedTB(mode='Verbose',
+#      color_scheme='Linux', call_pdb=1)
 
 ATOL=1e-2
-RTOL=1e-7
+RTOL=1e-4
 
 cuda = True
 verbose = True
@@ -43,25 +46,32 @@ def get_grads(nBatch=1, nz=10, neq=1, nineq=3, Qscale=1.,
               Gscale=1., hscale=1., Ascale=1., bscale=1.):
     assert(nBatch==1)
     npr.seed(1)
-    L = np.tril(np.random.randn(nz,nz))
+    L = np.random.randn(nz,nz)
     Q = Qscale*L.dot(L.T)
     G = Gscale*npr.randn(nineq,nz)
-    h = hscale*npr.randn(nineq)
+    # h = hscale*npr.randn(nineq)
+    z0 = npr.randn(nz)
+    s0 = npr.rand(nineq)
+    h = G.dot(z0)+s0
     A = Ascale*npr.randn(neq,nz)
-    b = bscale*npr.randn(neq)
+    # b = bscale*npr.randn(neq)
+    b = A.dot(z0)
 
     p = npr.randn(nBatch,nz)
     # print(np.linalg.norm(p))
     truez = npr.randn(nBatch,nz)
 
     Q, p, G, h, A, b, truez = [x.astype(np.float64) for x in [Q, p, G, h, A, b, truez]]
+    zhat, nu, lam = qp_cvxpy.forward_single_np(p[0], Q, G, h, A, b)
 
-    return [p[0], Q, G, h, A, b, truez], get_grads_torch(Q, p, G, h, A, b, truez)
+    grads = get_grads_torch(Q, p, G, h, A, b, truez)
+    return [p[0], Q, G, h, A, b, truez], grads
 
 def get_grads_torch(Q, p, G, h, A, b, truez):
     Q, p, G, h, A, b, truez = [torch.DoubleTensor(x) for x in [Q, p, G, h, A, b, truez]]
     if cuda:
         Q, p, G, h, A, b, truez = [x.cuda() for x in [Q, p, G, h, A, b, truez]]
+
     Q, p, G, h, A, b = [Variable(x) for x in [Q, p, G, h, A, b]]
     for x in [Q, p, G, h, A, b]: x.requires_grad = True
 
@@ -90,12 +100,12 @@ def test_dl_dp():
     if verbose:
         print('dp_fd: ', dp_fd)
         print('dp: ', dp)
-    #npt.assert_allclose(dp_fd, dp, rtol=RTOL, atol=ATOL)
+    npt.assert_allclose(dp_fd, dp[0], rtol=RTOL, atol=ATOL)
 
 def test_dl_dG():
-    nz, neq, nineq = 10, 0, 1
+    nz, neq, nineq = 10, 0, 3
     [p, Q, G, h, A, b, truez], [dQ, dp, dG, dh, dA, db] = get_grads(
-        nz=nz, neq=neq, nineq=nineq, Qscale=100., Gscale=100.)
+        nz=nz, neq=neq, nineq=nineq)
 
     def f(G):
         G = G.reshape(nineq,nz)
@@ -109,12 +119,13 @@ def test_dl_dG():
         # print('dG[1,:]: ', dG[1,:])
         print('dG_fd: ', dG_fd)
         print('dG: ', dG)
-    #npt.assert_allclose(dG_fd, dG, rtol=RTOL, atol=ATOL)
+    npt.assert_allclose(dG_fd, dG, rtol=RTOL, atol=ATOL)
+
 
 def test_dl_dh():
-    nz, neq, nineq = 2, 0, 3
+    nz, neq, nineq = 10, 0, 3
     [p, Q, G, h, A, b, truez], [dQ, dp, dG, dh, dA, db] = get_grads(
-        nz=nz, neq=neq, nineq=nineq, Qscale=1., Gscale=1., hscale=1.)
+        nz=nz, neq=neq, nineq=nineq, Qscale=1., Gscale=1.)
 
     def f(h):
         zhat, nu, lam = qp_cvxpy.forward_single_np(p, Q, G, h, A, b)
@@ -125,10 +136,10 @@ def test_dl_dh():
     if verbose:
         print('dh_fd: ', dh_fd)
         print('dh: ', dh)
-    #npt.assert_allclose(dp_fd, dp, rtol=RTOL, atol=ATOL)
+    npt.assert_allclose(dh_fd, dh, rtol=RTOL, atol=ATOL)
 
 def test_dl_dA():
-    nz, neq, nineq = 10, 1, 1
+    nz, neq, nineq = 10, 3, 1
     [p, Q, G, h, A, b, truez], [dQ, dp, dG, dh, dA, db] = get_grads(
         nz=nz, neq=neq, nineq=nineq, Qscale=100., Gscale=100., Ascale=100.)
 
@@ -144,7 +155,7 @@ def test_dl_dA():
         # print('dA[0,:]: ', dA[0,:])
         print('dA_fd: ', dA_fd)
         print('dA: ', dA)
-    #npt.assert_allclose(dA_fd, dA, rtol=RTOL, atol=ATOL)
+    npt.assert_allclose(dA_fd, dA, rtol=RTOL, atol=ATOL)
 
 def test_dl_db():
     nz, neq, nineq = 10, 3, 1
@@ -160,7 +171,7 @@ def test_dl_db():
     if verbose:
         print('db_fd: ', db_fd)
         print('db: ', db)
-    #npt.assert_allclose(dA_fd, dA, rtol=RTOL, atol=ATOL)
+    npt.assert_allclose(db_fd, db, rtol=RTOL, atol=ATOL)
 
 
 if __name__=='__main__':