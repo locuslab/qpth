98dcf1e679792293d41b3c893b5d1eb57d5b353e
Brandon Amos <bamos@cs.cmu.edu>
2017-04-10 22:47:06 -0400

    Bump to v0.0.6.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='qpth',
-    version='0.0.5',
+    version='0.0.6',
     description="A fast and differentiable QP solver for PyTorch.",
     author='Brandon Amos',
     author_email='bamos@cs.cmu.edu',
