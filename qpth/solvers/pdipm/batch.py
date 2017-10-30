942904516e9f0126f0c0bac2f92bb14a1fec5aea
Brandon Amos <bamos@cs.cmu.edu>
2017-10-30 16:40:50 +0000

    Add quick comment to the initialization.

--- qpth/solvers/pdipm/batch.py
+++ qpth/solvers/pdipm/batch.py
@@ -79,10 +79,12 @@ def forward(Q, p, G, h, A, b, Q_LU, S_LU, R, eps=1e-12, verbose=0, notImprovedLi
     else:
         assert False
 
+    # Make all of the slack variables >= 1.
     M = torch.min(s, 1)[0].repeat(1, nineq)
     I = M < 0
     s[I] -= M[I] - 1
 
+    # Make all of the inequality dual variables >= 1.
     M = torch.min(z, 1)[0].repeat(1, nineq)
     I = M < 0
     z[I] -= M[I] - 1
